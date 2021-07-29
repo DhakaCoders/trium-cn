23330081bf904b134245e4d23741bc16bc131b5b
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2021-07-30 00:39:50 +0600

    ,

--- themes/trium/assets/js/main.js
+++ themes/trium/assets/js/main.js
@@ -348,20 +348,6 @@ google.maps.event.addDomListener(window, 'load', initialize);
             slidesToShow: 3,
             slidesToScroll: 1
           }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 576,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
         }
         // You can unslick at a given breakpoint now by adding:
         // settings: "unslick"
